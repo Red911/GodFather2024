8445584c6b8b2c415c525014823981ffbb5e74fb
Romain <mromain911@gmail.com>
2023-09-07 10:55:38 +0200

    Decals

--- Assets/Script/RandomDecal.cs
+++ Assets/Script/RandomDecal.cs
@@ -6,42 +6,70 @@ using UnityEngine.Rendering.Universal;
 
 public class RandomDecal : MonoBehaviour
 {
-    [Header("Decals")]
+    [Header("List")]
     public Transform[] spawners;
     public GameObject[] decalPrefabs;
 
     [Header("Decals")]
     public GameObject decalPrefab;
-    public int nDecals = 2;
+    public float probability = 0.5f;
+    private int _nDecals = 1;
 
     [Range(0.1f, 0.7f)]
     public float minScale;
     [Range(0.1f, 0.7f)]
     public float maxScale;
-    
+
+    private Vector3 _lastPosition;
     
     void Start()
     {
+        RandDecals();
+    }
+
+    
+    void RandDecals()
+    {
+        _nDecals = RandIncreaseDecal(probability);
         
-        for (int i = 0; i <= nDecals; i++)
+        for (int i = 0; i <= _nDecals; i++)
         {
+            Transform decalPos = spawners[Random.Range(0, spawners.Length)];
+            
+            
+            if (decalPos.position == _lastPosition)
+            {
+                decalPos = spawners[Random.Range(0, spawners.Length)];
+            }
+            
+            _lastPosition = decalPos.position;
+            
             decalPrefab = decalPrefabs[Random.Range(0, decalPrefabs.Length)];
             
-            GameObject decalProjectorInst = Instantiate(decalPrefab, spawners[i].position, spawners[i].rotation);
+            GameObject decalProjectorInst = Instantiate(decalPrefab, decalPos);
+
+            
+            
             DecalProjector decalProjectorComp = decalProjectorInst.GetComponent<DecalProjector>();
 
             float scale = Random.Range(minScale, maxScale);
             
-            decalProjectorComp.size = new Vector3(scale, scale, 1f);
+            decalProjectorComp.size = new Vector3(scale, scale, 1.2f);
             
             decalProjectorComp.material = new Material(decalProjectorComp.material);
         }
-        
     }
 
-    
-    void Update()
+    int RandIncreaseDecal(float proba)
     {
+        int numberDecals = 0;
+        float rand = Random.value;
         
+        if (rand <= proba)
+        {
+            numberDecals += 1;
+        }
+        
+        return numberDecals;
     }
 }
